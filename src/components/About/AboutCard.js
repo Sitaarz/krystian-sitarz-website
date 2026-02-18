2e8b14d326b764823e3ed22f57adddf2b66de1d8
Krystian <sitarz.code@gmail.com>
2026-02-18 17:49:01 +0100

    Refactor aboutCard

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,15 +8,14 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Soumyajit Behera</span>{" "}
-            from <span className="purple">Bhubaneswar, India</span>.
+            Hi everyone! I’m <span className="purple">Krystian Sitarz</span>{" "}
+            from <span className="purple">Krakow, Poland</span>.
             <br />
-            I’m currently working as a{" "}
-            <span className="purple">Software Developer</span> at{" "}
-            <span className="purple">Juspay</span>.
-            <br />I hold an Integrated M.Sc. (IMSc) in{" "}
-            <span className="purple">Mathematics and Computing</span> from{" "}
-            <span className="purple">BIT Mesra</span>.
+            I’m currently a {" "}
+            <span className="purple">student</span> at{" "}
+            <span className="purple">AGH University of Krakow</span>.
+            <br />I am purchasing the M.Eng. in{" "}
+            <span className="purple">Computer Science and Intelligent Systems</span>
             <br />
             <br />
             Outside of coding, I love engaging in activities that keep me
@@ -25,20 +24,16 @@ function AboutCard() {
 
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games 🎮
+              <ImPointRight /> Ice scating ⛸
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs ✍️
+              <ImPointRight /> Weight lifting 🏋️‍♂️
             </li>
             <li className="about-activity">
               <ImPointRight /> Traveling and Exploring New Places 🌍
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
         </blockquote>
       </Card.Body>
     </Card>
