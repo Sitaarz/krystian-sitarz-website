f0fe8c8e82f6431da1009c9f3efaf90ef1a7b40a
Krystian <sitarz.code@gmail.com>
2026-02-18 17:49:40 +0100

    refactor type

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,8 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          ".NET Developer",
+          "Angular Developer",
         ],
         autoStart: true,
         loop: true,
